e063539533caa2705a457b779fcaa102cd4e61a3
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2020-05-21 09:39:28 +0900

    コンポーネント分割

--- components/UserList.vue
+++ components/UserList.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    <div v-for="(user, i) in users" :key="i">
-      <span> {{ user.name }} </span>
-      <span> {{ user.age }} </span>
-    </div>
+    <user-list-row v-for="(user, i) in users" :key="i" :user="user" />
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import UserListRow from '~/components/UserListRow.vue'
 
 export default Vue.extend({
+  components: {
+    UserListRow
+  },
   props: {
     users: {
       type: Array,
---
+++ components/UserListRow.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span> {{ user.name }} </span>
+    <span> {{ user.age }} </span>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+})
+</script>
+<style></style>
