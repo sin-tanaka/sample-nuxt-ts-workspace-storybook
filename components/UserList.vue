bd6cae6beadfd8b2fa6a91e398ac4b9d5277a8c6
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2020-05-21 09:36:39 +0900

    UserListを追加

---
+++ components/UserList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="(user, i) in users" :key="i">
+      <span> {{ user.name }} </span>
+      <span> {{ user.age }} </span>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    users: {
+      type: Array,
+      default: () => []
+    }
+  }
+})
+</script>
+<style></style>
---
+++ pages/users/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <user-list :users="users" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import UserList from '~/components/UserList.vue'
+
+type User = {
+  name: string
+  age: number
+}
+
+export default Vue.extend({
+  components: {
+    UserList
+  },
+  computed: {
+    // mock users
+    users: (): User[] => {
+      return [
+        { name: 'Giorno Giovana', age: 20 },
+        { name: 'John Mayer', age: 30 },
+        { name: 'Michel Jackson', age: 44440 }
+      ]
+    }
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: left;
+}
+</style>
